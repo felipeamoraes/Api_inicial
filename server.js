3533ef7b3630b901a50c9f69302347d09a6e70f0
Felipe Américo <126334791+felipeamoraes@users.noreply.github.com>
2026-04-20 20:33:00 -0300

    Use environment variable for server port

--- server.js
+++ server.js
@@ -77,8 +77,10 @@ app.get("/health", (req, res) => {
 });
 
 // Inicia servidor
-app.listen(3000, () => {
-  console.log("API rodando em http://localhost:3000");
+const PORT = process.env.PORT || 3000; // usa a porta do Render
+app.listen(PORT, () => {
+  console.log(`API rodando na porta ${PORT}`);
 });
+
 
 
